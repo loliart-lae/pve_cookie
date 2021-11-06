cd4b20008049faec5558f2e7a22986ca7ce7f30b
iVampireSP.com <im@ivampiresp.com>
2021-11-06 12:01:37 +0800

    Update cookie.php

--- cookie.php
+++ cookie.php
@@ -3,6 +3,10 @@ error_reporting(0);
 header('Access-Control-Allow-Origin: https://lightappengine.test');
 header('Access-Control-Allow-Credentials: true');
 
+foreach($_COOKIE as $key=>$value){
+ setCookie($key,"",time()-60);
+}
+
 $ticket = $_REQUEST['ticket'];
 
 header('Set-Cookie: PVEAuthCookie=' . urlencode($ticket) . ';SameSite=None; Secure');
